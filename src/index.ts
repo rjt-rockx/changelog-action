6896e3d6416c0257c8b2a8d2be8f065ceab95928
rjt <rjt.rockx@gmail.com>
2022-11-03 03:21:01 +0530

    Cleanup

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import fs from "node:fs";
+
+import { debug, getInput, setFailed, setOutput } from "@actions/core";
+
+async function run(): Promise<void> {
+	try {
+		debug(new Date().toTimeString());
+		const file: string = getInput("file");
+		const content: string = fs.readFileSync(file, "utf8");
+		debug(new Date().toTimeString());
+		setOutput("content", content);
+	} catch (error) {
+		if (error instanceof Error) setFailed(error.message);
+	}
+}
+
+run();
