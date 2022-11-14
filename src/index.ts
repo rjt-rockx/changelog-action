42c8562f2b6187fc8943e948ff4990797712479a
rjt <rjt.rockx@gmail.com>
2022-11-15 02:57:13 +0530

    Log to console for now

--- src/index.ts
+++ src/index.ts
@@ -5,23 +5,24 @@ async function run(): Promise<void> {
 	try {
 		debug(new Date().toTimeString());
 		const file: string = getInput("file");
-		const content: string = fs.readFileSync(file, "utf8");
-		debug(new Date().toTimeString());
+		console.log({ file });
+		// const content: string = fs.readFileSync(file, "utf8");
+		// debug(new Date().toTimeString());
 
-		/*
-		 * Steps:
-		 * 1. Parse the content of the markdown file
-		 * We need the version, a summary and a full description
-		 * 2. Send the content to Discord and Twitter
-		 * For Discord,
-		 * title: version
-		 * description: changelog trimmed to 2048 characters
-		 * For Twitter,
-		 * Changelog v(version) - (summary) --> trimmed to 280 characters
-		 * 3. Set the output and exit
-		 * */
+		// /*
+		//  * Steps:
+		//  * 1. Parse the content of the markdown file
+		//  * We need the version, a summary and a full description
+		//  * 2. Send the content to Discord and Twitter
+		//  * For Discord,
+		//  * title: version
+		//  * description: changelog trimmed to 2048 characters
+		//  * For Twitter,
+		//  * Changelog v(version) - (summary) --> trimmed to 280 characters
+		//  * 3. Set the output and exit
+		//  * */
 
-		setOutput("content", content);
+		// setOutput("content", content);
 	} catch (error) {
 		if (error instanceof Error) setFailed(error.message);
 	}
