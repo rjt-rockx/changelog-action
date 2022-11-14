436e14eadb4b81f1b4c1305dc50fe6f1ebfcc78b
rjt <rjt.rockx@gmail.com>
2022-11-15 02:45:45 +0530

    Update flow

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,20 @@ async function run(): Promise<void> {
 		const file: string = getInput("file");
 		const content: string = fs.readFileSync(file, "utf8");
 		debug(new Date().toTimeString());
+
+		/*
+		 * Steps:
+		 * 1. Parse the content of the markdown file
+		 * We need the version, a summary and a full description
+		 * 2. Send the content to Discord and Twitter
+		 * For Discord,
+		 * title: version
+		 * description: changelog trimmed to 2048 characters
+		 * For Twitter,
+		 * Changelog v(version) - (summary) --> trimmed to 280 characters
+		 * 3. Set the output and exit
+		 * */
+
 		setOutput("content", content);
 	} catch (error) {
 		if (error instanceof Error) setFailed(error.message);
